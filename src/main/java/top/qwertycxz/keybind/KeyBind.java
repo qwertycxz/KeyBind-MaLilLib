ad46f01f1b84fbdcc6ab9ba2499bc39e75555e34
qwertycxz <gmail2@qq.com>
2025-09-06 12:24:18 +0800

    chore: basic frame

---
+++ src/main/java/top/qwertycxz/keybind/KeyBind.java
@@ -0,0 +1,11 @@
+package top.qwertycxz.keybind;
+
+import net.fabricmc.api.DedicatedServerModInitializer;
+
+public class KeyBind implements DedicatedServerModInitializer {
+
+	@Override
+	public void onInitializeServer() {
+		throw new UnsupportedOperationException("Unimplemented method 'onInitializeServer'");
+	}
+}
